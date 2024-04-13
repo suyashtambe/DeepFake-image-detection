720c60b139791694bcc2dd3aa22f3ff98c0ff261
vaibhav7766 <vaibhavsharmav777@gmail.com>
2024-04-13 19:34:37 +0530

    updated flask functionalities

--- Flask_Webapp/main.py
+++ Flask_Webapp/main.py
@@ -1,46 +1,56 @@
-from flask import Flask, render_template,request
-from tensorflow.keras.models import load_model
+from flask import Flask, render_template, request
+from keras.layers import Flatten
+import pickle as pk
 import numpy as np
 import cv2
 
 app = Flask(__name__)
 
-def get_model(model_path):
-    model = load_model(model_path)
-    return model
+model = pk.load(open(r"Flask_webapp\models\rfr.pkl", "rb"))
 
-def preprocess_image(image_bytes):
-    nparr = np.frombuffer(image_bytes.read(), np.uint8)
-    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    image = np.expand_dims(image, axis=0)
-    image = cv2.resize(image, (256, 256))
-    image = image / 255.0
-    return image
+# def preprocess_image(image_path, image_size):
+#     image = cv2.imread(image_path)
+#     image = cv2.resize(image, image_size)
+#     image = image.astype(np.float32) / 255.0
+#     return np.expand_dims(image, axis=0)
 
-def predict_deepfake(image, path):
-    model = get_model(path)
-    image = preprocess_image(image)
-    predictions = model.predict(image)
-    return predictions
+# def predict_image(model, image_path, image_size):
+#     input_image = preprocess_image(image_path, image_size)
+#     prediction = model.predict(input_image)
+#     if prediction[0][0] >= 0.9:
+#         return "Real"
+#     else:
+#         return "Deep Fake"
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return render_template(r'index.html')
+    return render_template("index.html")
 
-@app.route('/results',methods =["GET", "POST"])
+
+@app.route("/results", methods=["GET", "POST"])
 def results():
-    context = {
-        "predictions": ""
-    }
-    
+    context = 0
     if request.method == "POST":
-        image = request.files['image']
-        # preds = predict_deepfake(image, 'models/deepfake_autoencoder.keras')
-        context["predictions"] = "These are predictions"
-        return render_template(r'results.html', context=context)
-    return render_template(r'results.html', context=context)
-    
+        try:
+            image_file = request.files["image"]
+            if image_file:
+                image = cv2.imdecode(
+                    np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR
+                )
+                image = cv2.resize(image, (224, 224))
+                image = image / 255.0
+                image_flat = image.flatten().reshape(1, -1)
+                predictions = model.predict(image_flat)
+                if predictions is not None:
+                    context = predictions[0]
+                    return render_template("results.html", context=context)
+        except Exception as e:
+            print(f"Error processing request: {e}")
+            context["predictions"] = "Error processing request"
+
+    return render_template("results.html", context=context)
 
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
--- Streamlit_Webapp/app.py
+++ Streamlit_Webapp/app.py
@@ -5,19 +5,18 @@ import cv2
 import numpy as np
 import base64
 
-# Load the trained model
-model = load_model(r'deepfake_autoencoder.keras')
+model = load_model(r"trained_fake_model.h5")
 
-# Function to get base64 encoding of a binary file
+
 def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
+    with open(bin_file, "rb") as f:
         data = f.read()
     return base64.b64encode(data).decode()
 
-# Function to set background image using base64 encoding
+
 def set_background(png_file):
     bin_str = get_base64(png_file)
-    page_bg_img = f'''
+    page_bg_img = f"""
     <style>
     .stApp {{
     background-image: url("data:image/png;base64,{bin_str}");
@@ -25,27 +24,16 @@ def set_background(png_file):
     backdrop-filter: blur(1000px);
     }}
     </style>
-    '''
+    """
     st.markdown(page_bg_img, unsafe_allow_html=True)
 
-# Set the background image
-set_background(r'Flask_Webapp\Templates\Images\bg_image.jpg')
 
-# Main title and description
-st.markdown("<h1 style='text-align: center; color: white; font-family: Arial, sans-serif;'>Deep Fake Image Detection using Multidisciplinary Approach</h1>", unsafe_allow_html=True)
-st.markdown("<p style='font-family: Arial, sans-serif; color: white; font-size: 18px; text-align: center;'>Collecting diverse dataset. Preprocess images. Extract attributes with CNNs. Split dataset. Select, train model. Monitor performance. Test for generalization.</p>", unsafe_allow_html=True)
-
-# Function to load and display Lottie animation
 def load_lottie_animation(json_file, width=None, height=None):
     with open(json_file, "r") as f:
         lottie_json = f.read()
     st_lottie(lottie_json, width=width, height=height)
 
-# Lottie animation
-# lottie_json_file = r"C:\Users\Suyash Tambe\Desktop\Deep-Fakee-\Animation - 1712299045488.json"  
-# load_lottie_animation(lottie_json_file, width=600, height=600)
 
-# Function to preprocess uploaded image
 def preprocess_image(image_bytes):
     nparr = np.frombuffer(image_bytes.read(), np.uint8)
     image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
@@ -53,27 +41,46 @@ def preprocess_image(image_bytes):
     image = image / 255.0
     return image
 
-# Function to predict deepfake from image
+
 def predict_deepfake(image, model):
     image = np.expand_dims(image, axis=0)
     predictions = model.predict(image)
     return predictions
 
-# File uploader for image with colored text
-st.subheader(':blue[Upload Image (JPG only):]')
-uploaded_file = st.file_uploader("Choose a JPG image...", type="jpg")
 
+def main():
+    set_background(r"Flask_Webapp\Templates\Images\bg_image.jpg")
 
-# Predict button
-if st.button("Predict", key="predict_button", help="Click to predict if the uploaded image is a deepfake or real."):
-    if uploaded_file is not None:
-        image = preprocess_image(uploaded_file)
-        predictions = predict_deepfake(image, model)
-        st.subheader(':blue[Prediction:]')
-        threshold = 0.5
-        if predictions[0][0] < threshold:
-            st.write("<p style='color: red; font-size: 20px;'>The image is DEEPFAKE.</p>", unsafe_allow_html=True)
+    st.markdown(
+        "<h1 style='text-align: center; color: white; font-family: Arial, sans-serif;'>Deep Fake Image Detection using Multidisciplinary Approach</h1>",
+        unsafe_allow_html=True,
+    )
+    st.markdown(
+        "<p style='font-family: Arial, sans-serif; color: white; font-size: 18px; text-align: center;'>Collecting diverse dataset. Preprocess images. Extract attributes with CNNs. Split dataset. Select, train model. Monitor performance. Test for generalization.</p>",
+        unsafe_allow_html=True,
+    )
+
+    st.subheader(":blue[Upload Image (JPG only):]")
+    uploaded_file = st.file_uploader("Choose a JPG image...", type="jpg")
+
+    if st.button(
+        "Predict",
+        key="predict_button",
+        help="Click to predict if the uploaded image is a deepfake or real.",
+    ):
+        if uploaded_file is not None:
+            image = preprocess_image(uploaded_file)
+            predictions = predict_deepfake(image, model)
+            st.subheader(":blue[Prediction:]")
+            threshold = 0.9
+            if predictions[0][0] < threshold:
+                st.error("The image is DEEPFAKE", icon="🚨")
+            if predictions[0][0] >= threshold:
+                st.success("The image is REAL", icon="✅")
+            st.write(predictions[0][0])
         else:
-            st.write("<p style='color: green; font-size: 20px;'>The image is REAL.</p>", unsafe_allow_html=True)
-    else:
-        st.write("<p style='color: orange; font-size: 18px;'>Please upload an image before predicting.</p>", unsafe_allow_html=True)
+            st.info("Please upload an image before predicting.", icon="⚠️")
+
+
+if __name__ == "__main__":
+    main()
